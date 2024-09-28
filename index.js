97d4490e8c13c1387fcb6ea90c052ec349d0467b
ihbrazil <ihbrazil@users.noreply.github.com>
2024-09-28 15:01:59 -0300

    Validação dos dados de entrada
    Validação dos dados antes do cadastro no banco de dados: O nome deve ser obrigatório;
    O nome deve ter pelo menos dois caracteres;
    O email deve ter um formato válido email@provedor.com;
    O telefone deve ter um formato válido (XX) XXXXX-XXXX;
    O sistema não precisa tratar dados duplicados. Ou seja, para esse contexto, não é um problema que a mesma pessoa se cadastre mais de uma vez. A mensagem de validação é na ordem dos campos: nome, email e telefone.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from "cors";
 
 import { cadastraLead } from './servico/cadastroServico.js';
+import { validaUsuario } from './validacao/valida.js';
 
 const app = express();
 
@@ -13,10 +14,14 @@ app.post("/usuarios", async (req, res) => {
     const email = req.body.email;
     const telefone = req.body.telefone;
 
+    const usuarioValido = validaUsuario(nome, email, telefone);
+
+    if (usuarioValido.status) {
         await cadastraLead(nome, email, telefone);
         res.status(204).end();
-    
-    
+    } else {
+        res.status(400).send({mensagem: usuarioValido.mensagem});
+    }
 });
 
 app.listen(9000, async () => {
